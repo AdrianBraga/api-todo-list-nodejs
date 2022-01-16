82e500f8c8ff65e38093dea283c6de3f306f4a5a
Adrian Braga <adrian.braga.ro@gmail.com>
2022-01-15 20:24:49 -0400

    Route from create new users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const res = require('express/lib/response');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const usernameAlreadyExists = users.some(user => user.username === username)
+
+  if (usernameAlreadyExists) return response.status(400).json({ error: 'Username Already Exists!' })
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+
+  return response.status(201).json({ message: 'User created successfully!' });
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
