6b774c1d4d7cfa60e0c00911c155bc57d897ca11
Adrian Braga <adrian.braga.ro@gmail.com>
2022-01-16 17:55:34 -0400

    Route from delete one todo

--- src/index.js
+++ src/index.js
@@ -105,7 +105,17 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const arrayTodos = user.todos;
+  const todoIndex = arrayTodos.findIndex(index => index.id === id);
+
+  if (todoIndex < 0) return response.status(404).json({ error: 'Id not found!' });
+  
+  arrayTodos.splice(todoIndex, 1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
